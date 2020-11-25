3f01d609b480645e8b8723d317c9854e57224a91
湯浅海貴 <yuasa.kshu@gmail.com>
2020-11-25 15:44:37 +0900

    [add] category filter

--- src/getTopics/index.js
+++ src/getTopics/index.js
@@ -6,59 +6,79 @@ const documentClient = new AWS.DynamoDB.DocumentClient({
 exports.handler = async (event) => {
   let response = {
     statusCode: 200,
-    body: '',
+    body: "",
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
     },
   };
   try {
+    let result = "";
     let params = {
       TableName: 'Teleto-topics',
     };
-    let result = '';
-    let resultJSON = await documentClient.scan(params).promise();
 
-    if (event.queryStringParameters !== null) {
-      if (event.queryStringParameters['random'] === 'true') {
+    let resultJSON = await documentClient.scan(params).promise();
+    if ('category' in event.queryStringParameters) {
+      let tmpList = {
+        "Items": [],
+        "Count": 0,
+        "ScannedCount": 0
+      };
+      for (let i = 0; i < resultJSON.Count; i++) {
+        for (let j = 0; j < resultJSON.Items[i].category.length; j++) {
+          if (resultJSON.Items[i].category[j] === event.queryStringParameters.category) {
+            tmpList.Items.push(resultJSON.Items[i]);
+            tmpList.Count++;
+            tmpList.ScannedCount++;
+          }
+        }
+      }
+      resultJSON = tmpList;
+    }
+
+    if ('random' in event.queryStringParameters) {
+      if (event.queryStringParameters['random'] === "true") {
         let random = Math.floor(Math.random() * resultJSON.Count);
 
         result = [resultJSON.Items[random]];
-
-        if ('option' in result[0].topic) {
+        if ("option" in result[0].topic) {
           let opt = [];
+
           for (let i = 0; i < result[0].topic.option.length; i++) {
-            if (result[0].topic.option[i] === 'members') {
+            if (result[0].topic.option[i] === "members") {
               params = {
                 TableName: 'Teleto-members',
                 ExpressionAttributeNames: { '#x': 'grouphash' },
                 ExpressionAttributeValues: { ':y': event.queryStringParameters['grouphash'] },
-                KeyConditionExpression: '#x = :y',
+                KeyConditionExpression: '#x = :y'
               };
-              let resultMember = '';
+              let resultMember = "";
               resultJSON = await documentClient.query(params).promise();
               random = Math.floor(Math.random() * resultJSON.Count);
 
               resultMember = [resultJSON.Items[random]];
               opt[i] = resultMember[0].membername;
-            } else if (result[0].topic.option[i] === 'trends') {
+            }
+            else if (result[0].topic.option[i] === "trends") {
               params = {
                 TableName: 'Teleto-trends',
               };
-              let resultTrend = '';
+              let resultTrend = "";
               resultJSON = await documentClient.scan(params).promise();
               random = Math.floor(Math.random() * resultJSON.Count);
 
               resultTrend = [resultJSON.Items[random]];
               opt[i] = resultTrend[0].name;
-            } else {
+            }
+            else {
               params = {
                 TableName: 'Teleto-options',
                 ExpressionAttributeNames: { '#x': 'groupname' },
                 ExpressionAttributeValues: { ':y': result[0].topic.option[i] },
-                KeyConditionExpression: '#x = :y',
+                KeyConditionExpression: '#x = :y'
               };
-              let resultOption = '';
+              let resultOption = "";
               resultJSON = await documentClient.query(params).promise();
               random = Math.floor(Math.random() * resultJSON.Count);
 
@@ -66,26 +86,30 @@ exports.handler = async (event) => {
               opt[i] = resultOption[0].word;
             }
           }
-          let string = eval('`' + result[0].topic.template + '`');
+          let string = eval("`" + result[0].topic.template + "`");
           result[0].topic.template = string;
 
-          const resbody = { value: result[0].topic.template };
+          const resbody = { "value": result[0].topic.template }
           response.body = JSON.stringify(resbody);
         }
-      } else {
+      }
+      else {
         result = resultJSON.Items;
         response.body = JSON.stringify(result);
       }
-    } else {
+    }
+    else {
       result = resultJSON.Items;
       response.body = JSON.stringify(result);
     }
 
     return response;
-  } catch (e) {
+  }
+  catch (e) {
     response.statusCode = 402;
-    response.body = 'Error: request schema is invalid.';
+    response.body = "Error: request schema is invalid."
     console.log(e);
     return response;
   }
+
 };
